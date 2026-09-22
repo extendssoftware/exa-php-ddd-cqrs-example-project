a401b4ef66faea5f01ecfa3da151428081503a24
Vincent van Dijk <vincent@extends.nl>
2026-09-22 10:59:20 +0200

    Merge `router.config.php` into `controllers.config.php` because it spans the same subject.

--- module/Application/config/controllers.config.php
+++ module/Application/config/controllers.config.php
@@ -2,11 +2,15 @@
 
 declare(strict_types=1);
 
+use ExtendsSoftware\ExaPHP\Router\RouterInterface;
 use ExtendsSoftware\ExaPHP\ServiceLocator\Resolver\Reflection\ReflectionResolver;
 use ExtendsSoftware\ExaPHP\ServiceLocator\ServiceLocatorInterface;
 use ExtendsSoftware\ExaPHPExample\Application\Infrastructure\Http\Controller\IndexController;
 
 return [
+    RouterInterface::class => [
+        IndexController::class,
+    ],
     ServiceLocatorInterface::class => [
         ReflectionResolver::class => [
             IndexController::class => IndexController::class,
