deafafe1b69bc298e2bf0176f551ea9c8bf61b0d
Vincent van Dijk <vincent@extends.nl>
2026-09-23 13:07:50 +0200

    Import `str_repeat` function in `TaskTest.php`.

--- module/Task/tests/Domain/Task/TaskTest.php
+++ module/Task/tests/Domain/Task/TaskTest.php
@@ -15,6 +15,8 @@ use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
 
+use function str_repeat;
+
 final class TaskTest extends TestCase
 {
     /**
